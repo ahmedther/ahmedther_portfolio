2a6331c25793c388e81a3f77ca7af9e5489baf30
Ahmed <aa10980@gmail.com>
2024-07-27 14:31:08 -0300

    fortunate

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
 
-if (process.env.HOSTING === "Firebase") {
-  nextConfig.output = "export";
-  nextConfig.images = {
-    unoptimized: true,
-  };
-} else {
-  nextConfig.redirects = async () => {
+const nextConfig = {
+  // output: "export",
+  // images: {
+  //   unoptimized: true,
+  // },
+
+  async redirects() {
     return [
       {
         source: "/guest",
@@ -17,27 +14,13 @@ if (process.env.HOSTING === "Firebase") {
         permanent: false, // Set to true for permanent redirects
       },
     ];
+  },
+};
+
+if (process.env.HOSTING === "Firebase") {
+  nextConfig.images = {
+    unoptimized: true,
   };
 }
-
-// if(process.env.HOSTING === 'production') {}
-
-// const nextConfig = {
-
-//   // output: "export",
-//   // images: {
-//   //   unoptimized: true,
-//   // },
-
-//   async redirects() {
-//     return [
-//       {
-//         source: "/guest",
-//         destination: "/",
-//         permanent: false, // Set to true for permanent redirects
-//       },
-//     ];
-//   },
-// };
 
 export default nextConfig;
